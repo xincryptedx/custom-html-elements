7139c6607e2684ab8b1eb5d9cb37b6713885dac5
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-17 19:20:35 -0400

    Remove extra drop down menus for now

--- src/index.js
+++ src/index.js
@@ -4,7 +4,3 @@ import DropDown from "./modules/dropDown/dropDown";
 const dropDownMenu = DropDown();
 
 document.body.appendChild(dropDownMenu.element);
-
-for (let i = 0; i < 10; i += 1) {
-  document.body.appendChild(DropDown().element);
-}
