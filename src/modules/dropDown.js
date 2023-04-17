a180b63ea60d9df656757ecbddf10ff230e7cb85
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-17 15:34:32 -0400

    Add some pseudo code

--- src/modules/dropDown.js
+++ src/modules/dropDown.js
@@ -13,9 +13,33 @@ const dropDown = ((passedOptions) => {
 
   // Drop down references
 
-  // Drop down properties
+  /* Drop down properties
+  --General values--
+   placeholder text
+   menu data which will be [value, value, value] or {category: [value], category: [value]}
+
+  --Function Variations--
+   autoSuggest bool
+   autoComplete bool
+   userSetValues bool
+   required bool
+
+  --Style Variations--
+   detatched bool
+   style options enum
+   style
+    */
 
   // Construct the element and children
+  // Create base html element that will contain everything
+  // label text
+  // container
+  //   arrow
+  //   selected option text/placeholder text
+  // dropdown menu
+  //   scroll bar
+  //   **menu items, cagetgories, and dividers created from menu data property**
+  // feedback text
 
   // Style the element and children
 
