d1d2b93ab2918f19b4a0872c3324ea3797569c18
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-18 14:20:42 -0400

    Create a drop down with invalid params for testing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import "./styles/style.css";
 import DropDown from "./modules/dropDown/dropDown";
 
-const dropDownMenu = DropDown();
+const dropDownMenu = DropDown({
+  parentClasses: ["custom-class-1", "custom-class-2"],
+});
+
+const invalidParamDD = DropDown({ parentClasses: null });
 
 document.body.appendChild(dropDownMenu.element);
+
+document.body.appendChild(invalidParamDD.element);
