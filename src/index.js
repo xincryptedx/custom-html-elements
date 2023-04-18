906870f33f7bfe51c5f957a60ba1ee070b511160
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-18 14:36:37 -0400

    Add params to dropDown and invalid drop down to test validation

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import DropDown from "./modules/dropDown/dropDown";
 
 const dropDownMenu = DropDown({
   parentClasses: ["custom-class-1", "custom-class-2"],
+  containerClasses: ["custom-class-1", "custom-class-2"],
+  selectionClasses: ["custom-class-1", "custom-class-2"],
+  labelClasses: ["custom-class-1", "custom-class-2"],
 });
 
-const invalidParamDD = DropDown({ parentClasses: null });
+const invalidParamDD = DropDown({
+  parentClasses: undefined,
+  containerClasses: 6,
+  selectionClasses: null,
+  labelClasses: NaN,
+});
 
 document.body.appendChild(dropDownMenu.element);
 
