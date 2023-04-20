7916a333f048be9e49e244649136594a49f7fc39
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-20 16:42:11 -0400

    Import and use new submodule in dropDown.js

--- src/modules/dropDown/dropDown.js
+++ src/modules/dropDown/dropDown.js
@@ -1,5 +1,6 @@
 import validateOptions from "./validateOptions";
 import validateMenuData from "./validateMenuData";
+import generateDefaultStyle from "./generateDefaultStyle";
 import arrowSvg from "./arrow.svg";
 
 const dropDown = (passedOptions, passedData) => {
@@ -31,26 +32,7 @@ const dropDown = (passedOptions, passedData) => {
 
   const validatedData = validateMenuData(defaultData, passedData);
 
-  // Default styles based on variable class names
-  const defaultStyles = `
-  .${validatedOptions.parentClasses.toString()} {
-  margin: 0;
-  padding: 0;
-  background-color: grey;
-}`;
-
-  // If default styles tag doesn't exist, create it and add rules for this drop down
-  // If it does exist, just append the rules
-  let defaultStylesTag = document.querySelector("#default-drop-down-styles");
-  if (!defaultStylesTag) {
-    defaultStylesTag = document.createElement("style");
-    defaultStylesTag.id = "default-drop-down-styles";
-    defaultStylesTag.innerHTML = defaultStyles;
-
-    document.body.appendChild(defaultStylesTag);
-  } else {
-    defaultStylesTag.innerHTML += defaultStyles;
-  }
+  generateDefaultStyle(validatedOptions);
 
   // #endregion
 
