ece1f687ed242e6085d8fb60b8b0b7c18f98aeeb
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-20 13:56:03 -0400

    Return default data instead of false if passed data is invalid

--- src/modules/dropDown/validateMenuData.js
+++ src/modules/dropDown/validateMenuData.js
@@ -1,8 +1,8 @@
 // This will return true if dataToValidate is an array containing at least one string
-const validateMenuData = (dataToValidate) => {
-  if (!Array.isArray(dataToValidate)) return false;
+const validateMenuData = (defaultData, passedData) => {
+  if (!Array.isArray(passedData)) return defaultData;
 
-  const validatedData = dataToValidate;
+  const validatedData = passedData;
 
   // Check each entry and remove non strings
   for (let i = 0; i < validatedData.length; i += 1) {
@@ -12,9 +12,9 @@ const validateMenuData = (dataToValidate) => {
   }
 
   // If array is now empty return false
-  if (validatedData.length === 0) return false;
+  if (validatedData.length === 0) return defaultData;
 
-  return true;
+  return validatedData;
 };
 
 export default validateMenuData;
