54aa6333f2188d3d4c1b1dffaab10c0c89048467
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-20 13:52:19 -0400

    Write method for validating that menu data is an array with at least one string

--- src/modules/dropDown/validateMenuData.js
+++ src/modules/dropDown/validateMenuData.js
@@ -1,3 +1,20 @@
-const validateMenuData = (dataToValidate) => {};
+// This will return true if dataToValidate is an array containing at least one string
+const validateMenuData = (dataToValidate) => {
+  if (!Array.isArray(dataToValidate)) return false;
+
+  const validatedData = dataToValidate;
+
+  // Check each entry and remove non strings
+  for (let i = 0; i < validatedData.length; i += 1) {
+    if (typeof validatedData[i] !== "string") {
+      validatedData[i].pop();
+    }
+  }
+
+  // If array is now empty return false
+  if (validatedData.length === 0) return false;
+
+  return true;
+};
 
 export default validateMenuData;
