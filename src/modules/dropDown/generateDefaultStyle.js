1237a86ee04552b3a547c9f2bd43a6e6bca05070
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-22 00:27:15 -0400

    Check if style tag includes rule before adding it

--- src/modules/dropDown/generateDefaultStyle.js
+++ src/modules/dropDown/generateDefaultStyle.js
@@ -2,23 +2,48 @@ const generateDefaultStyle = (validatedOptions) => {
   // Default styles based on variable class names
   const defaultParentStyle = `
   .${validatedOptions.parentClasses.toString()} {
-  margin: 0;
-  padding: 0;
   background-color: grey;
 }`;
 
+  const defaultContainerStyle = `
+.${validatedOptions.containerClasses.toString()} {
+  background-color: green;
+}`;
+
+  let stylesToAdd = defaultParentStyle + defaultContainerStyle;
+
+  /* parentClasses: "drop-down",
+    containerClasses: "drop-down-container",
+    labelClasses: "drop-down-label",
+    selectionClasses: "drop-down-selection",
+    arrowClasses: "drop-down-arrow",
+    menuClasses: "drop-down-menu",
+    scrollBarClasses: "drop-down-scroll-bar",
+    optGroupClasses: "drop-down-optgroup",
+    optionClasses: "drop-down-option",
+    dividerClasses: "drop-down-divider",
+    feedbackClasses: "drop-down-feedback", */
+
   // If default styles tag doesn't exist, create it and add rules for this drop down
-  // If it does exist, just append the rules
+  // If it does exist, just append the rules if they don't exist yet
   let defaultStylesTag = document.querySelector("#default-drop-down-styles");
   if (!defaultStylesTag) {
     defaultStylesTag = document.createElement("style");
     defaultStylesTag.id = "default-drop-down-styles";
-    defaultStylesTag.innerHTML = defaultParentStyle;
-
+    defaultStylesTag.innerHTML = stylesToAdd;
     document.head.appendChild(defaultStylesTag);
   } else {
-    // check if defaultstylestag has the rule in its inner html
-    defaultStylesTag.innerHTML += defaultParentStyle;
+    stylesToAdd = "";
+
+    if (
+      !defaultStylesTag.innerHTML.includes(
+        validatedOptions.parentClasses.toString()
+      )
+    ) {
+      stylesToAdd += defaultParentStyle;
+    }
+
+    defaultStylesTag.innerHTML += stylesToAdd;
   }
 };
 
