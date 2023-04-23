364a03f1c1f2147ee25bd63653e12818b3ec98f1
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-23 14:16:55 -0400

    Fix dropDownMenu params

--- src/index.js
+++ src/index.js
@@ -1,34 +1,21 @@
 import "./styles/style.css";
 import DropDown from "./modules/dropDown/dropDown";
 
-const dropDownMenu = DropDown(
-  {
-    parentClasses: ["custom-class-1", "custom-class-2"],
-    containerClasses: ["custom-class-3", "custom-class-4"],
-    selectionClasses: ["custom-class-5", "custom-class-6"],
-    labelClasses: ["custom-class-7", "custom-class-8"],
-    arrowClasses: "custom-class-9",
-    menuClasses: ["custom-class-10"],
-    scrollBarClasses: ["custom-class-11", "custom-class-12"],
-    feedbackClasses: "custom-class-13",
-    inputIdPrefix: "custom-prefix-",
-  },
-  [
-    "#Fruit",
-    "Apples",
-    "Bananas",
-    "Grapes",
-    "_",
-    "Tomatoes",
-    "Cucumbers",
-    "Bell Peppers",
-    "Corn",
-    "#Vegetables",
-    "Carrots",
-    "Potatoes",
-    "Lettuce",
-  ]
-);
+const dropDownMenu = DropDown([
+  "#Fruit",
+  "Apples",
+  "Bananas",
+  "Grapes",
+  "_",
+  "Tomatoes",
+  "Cucumbers",
+  "Bell Peppers",
+  "Corn",
+  "#Vegetables",
+  "Carrots",
+  "Potatoes",
+  "Lettuce",
+]);
 
 const invalidParamDD = DropDown(
   {
