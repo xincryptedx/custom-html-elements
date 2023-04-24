742a007a864a6c42fb999a2b625320d8cd3eb1ee
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-24 14:58:43 -0400

    Refactor to use style classes to generate style sheet

--- src/modules/dropDown/generateDefaultStyle.js
+++ src/modules/dropDown/generateDefaultStyle.js
@@ -1,50 +1,17 @@
-const generateDefaultStyle = (generatedStyleClasses) => {
-  // Default styles based on variable class names
+const generateDefaultStyle = (styleClasses) => {
   const defaultParentStyle = `
-  .${generatedStyleClasses.parentClasses.toString()} {
+  :host {
   background-color: grey;
 }`;
 
   const defaultContainerStyle = `
-.${generatedStyleClasses.containerClasses.toString()} {
+.${styleClasses.container} {
   background-color: green;
 }`;
 
-  let stylesToAdd = defaultParentStyle + defaultContainerStyle;
+  const styles = defaultParentStyle + defaultContainerStyle;
 
-  /* parentClasses: "drop-down",
-    containerClasses: "drop-down-container",
-    labelClasses: "drop-down-label",
-    selectionClasses: "drop-down-selection",
-    arrowClasses: "drop-down-arrow",
-    menuClasses: "drop-down-menu",
-    scrollBarClasses: "drop-down-scroll-bar",
-    optGroupClasses: "drop-down-optgroup",
-    optionClasses: "drop-down-option",
-    dividerClasses: "drop-down-divider",
-    feedbackClasses: "drop-down-feedback", */
-
-  // If default styles tag doesn't exist, create it and add rules for this drop down
-  // If it does exist, just append the rules if they don't exist yet
-  let defaultStylesTag = document.querySelector("#default-drop-down-styles");
-  if (!defaultStylesTag) {
-    defaultStylesTag = document.createElement("style");
-    defaultStylesTag.id = "default-drop-down-styles";
-    defaultStylesTag.innerHTML = stylesToAdd;
-    document.head.appendChild(defaultStylesTag);
-  } else {
-    stylesToAdd = "";
-
-    if (
-      !defaultStylesTag.innerHTML.includes(
-        generatedStyleClasses.parentClasses.toString()
-      )
-    ) {
-      stylesToAdd += defaultParentStyle;
-    }
-
-    defaultStylesTag.innerHTML += stylesToAdd;
-  }
+  return styles;
 };
 
 export default generateDefaultStyle;
