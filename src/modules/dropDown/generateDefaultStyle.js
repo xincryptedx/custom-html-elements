b198f1dc4a232c4c11d2dd51a27cf5df6ae62f82
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-24 15:28:53 -0400

    Add default style rules section for each element

--- src/modules/dropDown/generateDefaultStyle.js
+++ src/modules/dropDown/generateDefaultStyle.js
@@ -1,18 +1,85 @@
 const generateDefaultStyle = (styleClasses) => {
   const defaultParentStyle = `
-  :host {
-  background-color: grey;
-}
-  :host([hidden]) { 
-    display: none 
-  }`;
+    :host {
+      background-color: grey;
+    }
+    :host([hidden]) { 
+      display: none 
+    }
+  `;
 
-  const defaultContainerStyle = `
-.${styleClasses.container} {
-  background-color: green;
-}`;
+  const containerStyle = `
+    .${styleClasses.container} {
+      background-color: green;
+    }
+  `;
 
-  const styles = defaultParentStyle + defaultContainerStyle;
+  const labelStyle = `
+    .${styleClasses.label} {
+      background-color: red;
+    }
+  `;
+
+  const selectionStyle = `
+    .${styleClasses.selection} {
+      background-color: blue;
+    }
+  `;
+
+  const arrowStyle = `
+    .${styleClasses.arrow} {
+      background-color: yellow;
+    }
+  `;
+
+  const menuStyle = `
+    .${styleClasses.menu} {
+      background-color: lightblue;
+    }
+  `;
+
+  const scrollbarStyle = `
+    .${styleClasses.scrollbar} {
+      background-color: lightgreen;
+    }
+  `;
+
+  const optgroupStyle = `
+    .${styleClasses.optgroup} {
+      background-color: pink;
+    }
+  `;
+
+  const optionStyle = `
+    .${styleClasses.option} {
+      background-color: purple;
+    }
+  `;
+
+  const dividerStyle = `
+    .${styleClasses.divider} {
+      border-bottom: 1px solid blue;
+    }
+  `;
+
+  const feedbackStyle = `
+    .${styleClasses.feedback} {
+      background-color: grey;
+    }
+  `;
+
+  const styles =
+    defaultParentStyle +
+    containerStyle +
+    labelStyle +
+    selectionStyle +
+    arrowStyle +
+    menuStyle +
+    scrollbarStyle +
+    optgroupStyle +
+    optionStyle +
+    dividerStyle +
+    feedbackStyle;
 
   return styles;
 };
