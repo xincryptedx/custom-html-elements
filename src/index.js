7b070d5bb13b83f7e868ee7924c253acafb96147
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-30 17:39:25 -0400

    Add img sources for slider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import "./styles/style.css";
 import DropDown from "./modules/dropDown/dropDown";
 import ImageSlider from "./modules/imageSlider/imageSlider";
 
+const imgSources = [
+  "https://drive.google.com/uc?id=18hvWeohZazFN_d8FrOEoPQLwFICyC3bl",
+  "https://drive.google.com/uc?id=1EYFWnbD-GqFwi7q7Nuwa9CFs_70jHvt5",
+  "https://drive.google.com/uc?id=18WJhHKkeFXzokEtcjVZewjYbTEi1_dhg",
+  "https://drive.google.com/uc?id=1MlFp-syap-h1S0NlL0LjNlQA6WYppQtp",
+  "https://drive.google.com/uc?id=1f0XH6oJJvnTipPTJuHlf4y57rTxIq3AG",
+];
+
 const dropDownMenu = DropDown(
   [
     "#Fruit",
@@ -46,16 +54,6 @@ document.body.appendChild(thirdDropDown.parent);
 
 dropDownMenu.zIndex = 300;
 
-const slider = ImageSlider([
-  234234,
-  null,
-  undefined,
-  false,
-  "asdfa",
-  "KLJDFKLJ3234kl@!#$@",
-  null,
-  2343,
-  {},
-]);
+const slider = ImageSlider(imgSources);
 
 document.body.appendChild(slider.parent);
