d8327f54820ed6b476f3a0d338f104f05be1da17
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-04-30 13:11:38 -0400

    Add pseudocode for creating parent html element

--- src/modules/imageSlider/imageSlider.js
+++ src/modules/imageSlider/imageSlider.js
@@ -1,3 +1,32 @@
-const imageSlider = () => {};
+const imageSlider = () => {
+  // #region Method for creating and returning html parent element
+  /*
+    Helper functions
+        -createImageSlides(sources, reel) = Go through sources and if valid format 
+         add an image element, set its src, classes, and append to the reel.
+
+        -createNavCircles(container) = For each img element, add a nav circle, set its data-index, 
+         classes, append to container, and set its click event to JumpTo. 
+
+    Static HTML
+        -main container
+            -imagesContainer
+                -imagesReel
+                    -*slides*(createImageSlides)
+            -previousBtnContainer
+                -img of arrow
+            -nextBtnContainer
+                -img of arrow
+            -navCirclesContainer
+                -*navCircles*(createNavCircles)
+  */
+  // #endregion
+  // #region Next and Previous functions for changing picture div position
+  // #endregion
+  // #region JumpTo function for nav circles
+  // #endregion
+  // #region Auto advance slide every x seconds
+  // #endregion
+};
 
 export default imageSlider;
